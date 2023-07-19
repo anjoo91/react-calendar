4baf3d68d07b24cc421049d090f8e87c82c50855
root <root@Xubuntu.myguest.virtualbox.org>
2023-07-19 16:34:35 -0400

    initial commit, set up files, added days.js and stubbed app.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import days, { dates }from '../days';
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className="app">
+    <h1>Calendar</h1>
+    <Calendar days={days} dates={dates}/> {/* render calendar component w. days & dates props */}
+    </div>
+  );
+};
---
+++ src/days.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+const days = [
+    {
+        name: "Sunday"
+    },
+    {
+        name: "Monday"
+    },
+    {
+        name: "Tuesday"
+    },
+    {
+        name: "Wednesday"
+    },
+    {
+        name: "Thursday"
+    },
+    {
+        name: "Friday"
+    },
+    {
+        name: "Saturday"
+    },
+]
+
+// The following creates an array of numbers from [1..28]
+const dates = Array.from({length: 28}, (x, i) => i + 1)
+
+
+export default days;
+export {dates};
